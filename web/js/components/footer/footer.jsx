49d5a23ab763f332a8f5ef1f98bf124e5610a0fb
Christer Olsen <colsen1991@gmail.com>
2016-04-23 18:09:06 +0200

    Fixed external links.

--- web/js/components/footer/footer.jsx
+++ web/js/components/footer/footer.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router';
 import styles from './footer.styl';
 
 export default () => (
   <footer className={styles.footer}>
     <span>Copyright © 2016 Christer Olsen</span>
     <span className={styles.images}>
-      <Link to="https://www.facebook.com/colsen1991"><img src="/img/facebook.png" alt="Facebook"/></Link>
-      <Link to="https://plus.google.com/100503672047908745160"><img src="/img/gplus.png" alt="Google+"/></Link>
-      <Link to="https://github.com/colsen1991"><img src="/img/github.png" alt="GitHub"/></Link>
-      <Link to="https://twitter.com/colsen1991"><img src="/img/twitter.png" alt="Twitter"/></Link>
-      <Link to="https://www.linkedin.com/in/christer-olsen-15845b53"><img src="/img/linkedin.png" alt="LinkedIn"/></Link>
+      <a target="_blank" href="https://www.facebook.com/colsen1991"><img src="/img/facebook.png" alt="Facebook"/></a>
+      <a target="_blank" href="https://plus.google.com/100503672047908745160"><img src="/img/gplus.png" alt="Google+"/></a>
+      <a target="_blank" href="https://github.com/colsen1991"><img src="/img/github.png" alt="GitHub"/></a>
+      <a target="_blank" href="https://twitter.com/colsen1991"><img src="/img/twitter.png" alt="Twitter"/></a>
+      <a target="_blank" href="https://www.linkedin.com/in/christer-olsen-15845b53"><img src="/img/linkedin.png" alt="LinkedIn"/></a>
     </span>
   </footer>
 );
